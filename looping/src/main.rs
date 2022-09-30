199c40477c446814ffef3de43da54a42f74a093f
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 15:53:30 +0200

    looping test 2

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     while response != user_input {
         user_input = "".to_string();
-        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?I don't know");
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         stdin().read_line(&mut user_input);
         attemps += 1;
     }
