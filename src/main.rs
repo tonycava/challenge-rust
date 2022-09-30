7b2fe9e6613cff60a4e5194dc0f9cee79e389633
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 15:22:31 +0200

    second exo test 2

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use temperature_conv::*;
 
 fn main() {
     println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+
+    println!("{} F = {} C", 20, celsius_to_fahrenheit(27.0));
+
     println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
 }
