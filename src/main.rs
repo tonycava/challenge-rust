734ad6587cce1e355267fc9aa35b9f744789d26b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 15:10:03 +0200

    first exo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use scalar::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("rem : {}", rem(22.0, 2.0));// 'rem : 0'
+    println!("rem : {}", rem(-128.23, 2.0));// 'rem : -0.22999573'
 }
