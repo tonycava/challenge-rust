369df3e57e2b618c2c7744d69e2f0911d43692d2
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-01 22:08:54 +0200

    arrange_it test 9

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,18 +1,18 @@
 pub fn arrange_phrase(phrase: &str) -> String {
     let list_of_word: Vec<&str> = phrase.split(" ").collect();
-    let mut to_return = String::new();
     let mut vec = Vec::new();
+    let mut end: Vec<String> = Vec::new();
 
     for i in 1..list_of_word.len() + 1 {
         let condition = i.to_string();
-        let to_add = list_of_word.iter().position(|&r| r.contains(&condition)).unwrap();
-        vec.push(to_add)
+        vec.push(list_of_word.iter().position(|&r| r.contains(&condition)).unwrap())
     }
 
     for i in vec.iter() {
         let word: String = list_of_word[*i].replace(&['1', '2', '3', '4', '5', '6', '7', '8', '9', ], "");
-        to_return += &format!("{} ", word);
+        end.push(word)
     }
 
-    return to_return.trim().to_string();
+
+    return end.join(" ");
 }
