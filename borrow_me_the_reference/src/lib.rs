dfce515888c7723c27441db11e56d00ded85b163
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-01 20:01:28 +0200

    borrow me the reference test 1

---
+++ borrow_me_the_reference/src/lib.rs
@@ -0,0 +1,67 @@
+pub fn delete_and_backspace(s: &mut String) {
+    let mut idx = 0;
+
+    while s.contains('-') || s.contains('+') {
+        let char = s.chars().nth(idx).unwrap();
+        if char == '-' && idx < s.len() {
+            s.remove(idx);
+            s.remove(idx - 1);
+            idx = 0;
+        } else if char == '+' && idx < s.len() {
+            let mut passHere = false;
+            s.remove(idx);
+
+            while s.chars().nth(idx).unwrap() == '+' {
+                passHere = true;
+                idx += 1;
+            }
+
+            if passHere {
+                s.remove(idx + 1);
+            } else {
+                s.remove(idx);
+            }
+            idx = 0;
+        }
+        idx += 1;
+    }
+}
+
+pub fn is_correct(v: &mut Vec<&str>) -> usize {
+    let mut number_of_good_response = 0;
+    for (idx, calc) in v.to_owned().iter().enumerate() {
+        let index_of_equal = calc.find('=').unwrap();
+        let attemped_result = &calc[index_of_equal + 1..];
+        let index_of_operator = calc.find('+').unwrap_or_else(|| calc.find('-').unwrap());
+        let operator = calc.chars().nth(index_of_operator).unwrap();
+
+        let first_number_string = &calc[0..index_of_operator];
+        let second_number_string = &calc[index_of_operator + 1..index_of_equal];
+
+        let first_number = first_number_string.parse::<i32>().unwrap();
+        let second_number = second_number_string.parse::<i32>().unwrap();
+
+        if operator == '+' {
+            let result = (first_number + second_number).to_string();
+            if result == attemped_result {
+                v[idx] = "✔";
+                number_of_good_response += 1;
+            } else {
+                v[idx] = "✘";
+            }
+        }
+
+        if operator == '-' {
+            let result = (first_number - second_number).to_string();
+            if result == attemped_result {
+                v[idx] = "✔";
+                number_of_good_response += 1;
+            } else {
+                v[idx] = "✘";
+            }
+        }
+    }
+
+    let response = number_of_good_response as f64 / v.len() as f64 * 100.0;
+    return response as usize;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use copy::*;
+use borrow_me_the_reference::{delete_and_backspace, is_correct};
 
 fn main() {
-    let a: i32 = 0;
-    let b = String::from("1 2 4 5 6");
-    let c = vec![1, 2, 4, 5];
+    let mut a = String::from("bpp--o+er+++sskroi-++lcw");;
 
-    println!("{:?}", nbr_function(a));
-    println!("{:?}", nbr_function(-12));
-    println!("{:?}", str_function(b));
-    println!("{:?}", vec_function(c));
+    let mut b: Vec<&str> = vec!["2+2=4", "3+2=5", "10-3=3", "5+5=10"];
+
+    // - If a value does **not implement Copy**, it must be **borrowed** and so will be passed by **reference**.
+    delete_and_backspace(&mut a); // the reference of  the value
+    let per = is_correct(&mut b); // the reference of  the value
+
+    println!("{:?}", (a, b, per));
+    // output: ("borrow", ["✔", "✔", "✘", "✔"], 75)
 }
