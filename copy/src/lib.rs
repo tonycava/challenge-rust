7c0afda8b6cd43e78259ef73039d7b5ae3c5249e
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-01 17:29:18 +0200

    copy test 2

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
-    return (c, (c as f64).exp(), (c as f64).log10());
+    return (c, (c as f64).exp(), (c as f64).log10().abs());
 }
 
 pub fn str_function(a: String) -> (String, String) {
@@ -17,7 +17,7 @@ pub fn str_function(a: String) -> (String, String) {
 pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
     let mut log: Vec<f64> = vec![];
     for nbr in b.iter() {
-        log.push((*nbr as f64).log10())
+        log.push((*nbr as f64).log10().abs())
     }
     return (b, log)
 }
