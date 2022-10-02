8c63f9422436552b83cd5248e1bad00fa61b4c30
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-02 23:37:56 +0200

    card deck test 2

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,6 +1,5 @@
-use std::borrow::Borrow;
+use std::ops::Range;
 use rand::Rng;
-use crate::Rank::Queen;
 
 #[derive(Debug)]
 pub struct Card {
@@ -8,7 +7,7 @@ pub struct Card {
     pub rank: Rank,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Suit {
     Heart,
     Diamond,
@@ -16,7 +15,7 @@ pub enum Suit {
     Club,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Rank {
     Ace(u8),
     King(u8),
@@ -26,7 +25,8 @@ pub enum Rank {
 
 impl Suit {
     pub fn random() -> Suit {
-        let rng = rand::thread_rng().gen_range(1..4);
+        let mut rng = rand::thread_rng().gen_range(1, 4);
+        println!("{} rng", rng);
         match rng {
             1 => Suit::Heart,
             2 => Suit::Diamond,
@@ -44,15 +44,18 @@ impl Suit {
         }
     }
 
-    pub fn get_suit(&self) -> &Suit {
+    pub fn get_suit(self) -> Suit {
         return self;
     }
 }
 
 impl Rank {
     pub fn random() -> Rank {
-        let which_rank = rand::thread_rng().gen_range(1..4);
-        let which_number = rand::thread_rng().gen_range(2..10);
+        // let which_rank: u8 = Range::new(1, 4);
+        // let which_number: u8 = Range::new(0, 10);
+        let mut which_rank = rand::thread_rng().gen_range(1, 4);
+        let mut which_number = rand::thread_rng().gen_range(2, 10);
+
         match which_rank {
             1 => Rank::Ace(which_number),
             2 => Rank::King(which_number),
@@ -70,13 +73,23 @@ impl Rank {
         }
     }
 
-    pub fn get_rank(&self) -> &Rank {
+    pub fn get_rank(self) -> Rank {
         return self;
     }
 }
 
 pub fn winner_card(card: Card) -> bool {
-    if card.rank.get_rank() == Rank::Ace && card.suit.get_suit() == Suit::Spade {
+    if (card.rank.get_rank() == Rank::Ace(2)
+        || card.rank.get_rank() == Rank::Ace(3)
+        || card.rank.get_rank() == Rank::Ace(4)
+        || card.rank.get_rank() == Rank::Ace(5)
+        || card.rank.get_rank() == Rank::Ace(6)
+        || card.rank.get_rank() == Rank::Ace(7)
+        || card.rank.get_rank() == Rank::Ace(8)
+        || card.rank.get_rank() == Rank::Ace(9)
+        || card.rank.get_rank() == Rank::Ace(10))
+
+        && card.suit.get_suit() == Suit::Spade {
         return true;
     }
     return false;
