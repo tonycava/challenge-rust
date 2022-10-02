288ef2f1daff40b2a00a929485b0589b574b7b0a
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-02 23:40:26 +0200

    card deck test 2

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,4 +1,3 @@
-use std::ops::Range;
 use rand::Rng;
 
 #[derive(Debug)]
@@ -17,10 +16,10 @@ pub enum Suit {
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Rank {
-    Ace(u8),
-    King(u8),
-    Queen(u8),
-    Jack(u8),
+    Ace,
+    King,
+    Queen,
+    Jack,
 }
 
 impl Suit {
@@ -44,7 +43,7 @@ impl Suit {
         }
     }
 
-    pub fn get_suit(self) -> Suit {
+    pub fn get_suit(&self) -> &Suit {
         return self;
     }
 }
@@ -57,39 +56,29 @@ impl Rank {
         let mut which_number = rand::thread_rng().gen_range(2, 10);
 
         match which_rank {
-            1 => Rank::Ace(which_number),
-            2 => Rank::King(which_number),
-            3 => Rank::Queen(which_number),
-            _ => Rank::Jack(which_number)
+            1 => Rank::Ace,
+            2 => Rank::King,
+            3 => Rank::Queen,
+            _ => Rank::Jack
         }
     }
 
     pub fn translate(value: u8) -> Rank {
         match value {
-            1 => Rank::Ace(1),
-            12 => Rank::Queen(1),
-            13 => Rank::King(1),
-            _ => Rank::Jack(1)
+            1 => Rank::Ace,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => Rank::Jack
         }
     }
 
-    pub fn get_rank(self) -> Rank {
+    pub fn get_rank(&self) -> &Rank {
         return self;
     }
 }
 
 pub fn winner_card(card: Card) -> bool {
-    if (card.rank.get_rank() == Rank::Ace(2)
-        || card.rank.get_rank() == Rank::Ace(3)
-        || card.rank.get_rank() == Rank::Ace(4)
-        || card.rank.get_rank() == Rank::Ace(5)
-        || card.rank.get_rank() == Rank::Ace(6)
-        || card.rank.get_rank() == Rank::Ace(7)
-        || card.rank.get_rank() == Rank::Ace(8)
-        || card.rank.get_rank() == Rank::Ace(9)
-        || card.rank.get_rank() == Rank::Ace(10))
-
-        && card.suit.get_suit() == Suit::Spade {
+    if card.rank.get_rank() == &Rank::Ace && card.suit.get_suit() == &Suit::Spade {
         return true;
     }
     return false;
