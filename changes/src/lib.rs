1ec3ea308b02317248fdc2898e7b98585a36c23a
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-02 16:51:27 +0200

    circle test 1

--- changes/src/lib.rs
+++ changes/src/lib.rs
@@ -20,6 +20,7 @@ pub fn change_brightness(lights: &mut Vec<Light>, alias: &str, value: u8) {
 
     let pos = lights.iter().position(|r| r.alias == alias).unwrap_or_else(|| 20);
     if pos != 20 {
+
         lights[pos].brightness = value;
     }
 }
---
+++ circle/src/lib.rs
@@ -0,0 +1,50 @@
+use std::f64::consts::PI;
+
+#[derive(Debug)]
+pub struct Circle {
+    pub center: Point,
+    pub radius: f64,
+}
+
+#[derive(Debug)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Point {
+    pub fn distance(&self, other: &Point) -> f64 {
+        let first = (other.x + self.x).powi(2);
+        let second = (other.y + self.y).powi(2);
+        return (first + second).sqrt();
+    }
+}
+
+
+impl Circle {
+    pub fn new(x: f64, y: f64, radius: f64) -> Self {
+        Circle {
+            center: {
+                Point {
+                    x,
+                    y,
+                }
+            },
+            radius,
+        }
+    }
+    pub fn area(&self) -> f64 {
+        return PI * self.radius * self.radius;
+    }
+
+    pub fn diameter(&self) -> f64 {
+        self.radius * 2.0
+    }
+
+    pub fn intersect(self, other: &Circle) -> bool {
+        if self.center.x + self.radius < other.center.x + other.radius && self.center.y + self.radius < other.center.y + other.radius {
+            return true;
+        }
+        return false;
+    }
+}
