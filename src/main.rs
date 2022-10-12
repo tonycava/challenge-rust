51d9d7afe72824773d9c1ef540d14cac3ca65faf
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-12 20:53:25 +0200

    capitalizing test 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
-use arrays::*;
+use capitalizing::*capitalizing*;
 
 fn main() {
-    let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let a1 = (1..11).collect::<Vec<i32>>();
-    let b = [5; 10]; //missing info here
-
-    println!("{:?}",  a1);
-    println!("{:?}",  a);
-
-    println!("The Sum of the elements in {:?} = {}", a, sum(&a));//missing info here
-
-    println!("The Sum of the elements in {:?} = {}", a1, sum(&a1));//missing info here
-
-    println!("The Sum of the elements in {:?} = {}", b, sum(&b));//missing info here
-
-    println!(
-        "Array size {} with only 10's in it {:?}",
-        thirtytwo_tens().len(),
-        thirtytwo_tens()
-    );
+    //println!("{}", capitalize_first("joe is missing"));
+    //println!("{}", title_case("jill is leaving A"));
+    println!("{}",change_case("heLLo THere"));
 }
