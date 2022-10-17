bfe94e7a312d6dc364328a765d5f192d29d2ba7b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-17 21:00:09 +0200

    cipher test 1

---
+++ cipher/src/lib.rs
@@ -0,0 +1,45 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CipherError {
+    validation: bool,
+    expected: String,
+}
+
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+const REVERSE_ALPHABET: &str = "zyxwvutsrqponmlkjihgfedcba";
+
+impl CipherError {
+    pub fn new(validation: bool, expected: String) -> CipherError {
+        CipherError {
+            validation,
+            expected,
+        }
+    }
+}
+
+fn encode(encode: &str) -> String {
+    let mut to_return = String::new();
+    let mut is_upper = false;
+    for word in encode.chars() {
+        if word.is_ascii_alphabetic() {
+            if word.is_uppercase() { is_upper = true }
+            let to_search = word.to_lowercase().nth(0).unwrap();
+            let i = ALPHABET.chars().position(|w| w == to_search).unwrap();
+            if is_upper {
+                to_return += &REVERSE_ALPHABET.chars().nth(i).unwrap().to_uppercase().to_string();
+                is_upper = false
+            } else { to_return += &REVERSE_ALPHABET.chars().nth(i).unwrap().to_string(); }
+            continue;
+        }
+        to_return += &word.to_string();
+    }
+    to_return.to_string()
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Option<Result<bool, CipherError>> {
+    if ciphered == encode(original) {
+        return Some(Ok(true));
+    } else if original != "" && ciphered != "" {
+        return Some(Err(CipherError::new(false, encode(original))));
+    }
+    return None;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use banner::*;
-use std::collections::HashMap;
+use cipher::*;
 
 fn main() {
-    let mut handler = FlagsHandler { flags: HashMap::new() };
-
-    let d = Flag::opt_flag("division", "divides the values, formula (a / b)");
-    let r = Flag::opt_flag(
-        "remainder",
-        "remainder of the division between two values, formula (a % b)",
-    );
-    
-    handler.add_flag((d.short_hand, d.long_hand), div);
-    handler.add_flag((r.short_hand, r.long_hand), rem);
-    
-    println!("{:?}", handler.exec_func(("-d".to_string(), "--division".to_string()), &["1.0", "2.0"]));
-    
-    // println!("{:?}",handler.exec_func(("-r".to_string(), "--remainder".to_string()), &["2.0", "2.0"]));
-    
-    // println!("{:?}",handler.exec_func(("-d".to_string(), "--division".to_string()), &["a", "2.0"]));
-    
-    // println!("{:?}",handler.exec_func(("-r".to_string(), "--remainder".to_string()), &["2.0", "fd"]));
+    println!("{:?}", cipher("1Hello 2world!", "1Svool 2dliow!"));
+    println!("{:?}", cipher("1Hello 2world!", "svool"));
+    println!("{:?}", cipher("", "svool"));
 }
