6dc7812db955ccfff8c1fa1e15a91c2a5788792b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-17 14:13:04 +0200

    unwrap or expect test 8

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub enum Security {
     Unknown,
     High,
@@ -7,11 +8,13 @@ pub enum Security {
 }
 pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
     println!("{:?}",server);
+    println!("{:?}",security_level);
+
     if server.clone().err() != None {
         match security_level {
             Security::Unknown => panic!("called `Result::unwrap()` on an `Err` value: \"ERROR CRITICAL\""),
             Security::BlockServer => panic!("ERROR: program stops"),
-            Security::Low => panic!("Not found: {}", server.clone().err().unwrap_or("".to_string())),
+            Security::Low => panic!("Not found: {}", server.unwrap_err()),
             Security::Medium => panic!("WARNING: check the server"),
             Security::High => panic!("ERROR: program stops"),
         }
