7cc769fe6cecb565bfa78fe6cd0be6fba8dc93d3
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-17 22:22:54 +0200

    error types test 1

---
+++ error_types/src/lib.rs
@@ -0,0 +1,87 @@
+use chrono::{NaiveDate, Utc};
+use regex::Regex;
+// this will be the structure that wil handle the errors
+#[derive(Debug, Eq, PartialEq)]
+pub struct FErr {
+    form_values: (String, String),
+    date: String,
+    err: String,
+}
+
+
+impl FErr {
+    pub fn new(form_values: (String, String), date: String, err: String) -> FErr {
+        FErr {
+            form_values,
+            date,
+            err,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Color {
+    Red,
+    Blue,
+    Green,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub first_name: String,
+    pub last_name: String,
+    pub birth: NaiveDate,
+    pub fav_colour: Color,
+    pub birth_location: String,
+    pub password: String,
+}
+
+#[allow(dead_code)]
+pub fn create_date(date: &str) -> NaiveDate {
+    NaiveDate::parse_from_str(date, "%Y-%m-%d").unwrap()
+}
+
+
+
+impl Form {
+    pub fn new(
+        first_name: String,
+        last_name: String,
+        birth: NaiveDate,
+        fav_colour: Color,
+        birth_location: String,
+        password: String,
+    ) -> Form {
+        Form {
+            birth,
+            password,
+            first_name,
+            last_name,
+            fav_colour,
+            birth_location,
+        }
+    }
+
+    pub fn validate(&self) -> Result<Vec<&str>, FErr> {
+        let passwd: String = String::from(&self.password);
+        if self.password.len() < 8 {
+            return Err(FErr::new((String::from(&self.first_name), passwd), Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(), "At least 8 characters".to_string()));
+        } else if self.first_name == "" {
+            return Err(FErr::new((String::from("first_name"), String::from("")), Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(), "No user name".to_string()));
+        } else if is_bad_passwd(&passwd) {
+            return Err(FErr::new((String::from("password"), passwd), Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(), "Combination of different ASCII character types (numbers, letters and none alphanumeric characters)".to_string()));
+        }
+        Ok(vec!["Valid first name", "Valid password"])
+    }
+}
+
+fn is_bad_passwd(passwd: &String) -> bool {
+    let nbr = Regex::new(r"[0-9]").unwrap();
+    let letter = Regex::new(r"[A-Za-z]").unwrap();
+    let syb = Regex::new(r"[\(_<>&/\\]").unwrap();
+
+    if nbr.is_match(&passwd) && letter.is_match(&passwd) && syb.is_match(&passwd) {
+        return false
+    }
+    true
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-use cipher::*;
+use error_types::*;
+pub use chrono::*;
 
 fn main() {
-    println!("{:?}", cipher("1Hello 2world!", "1Svool 2dliow!"));
-    println!("{:?}", cipher("1Hello 2world!", "svool"));
-    println!("{:?}", cipher("", "svool"));
+    let mut form_output = Form::new(
+        String::from("Lee"),
+        String::from("Silva"),
+        create_date("2015-09-05"),
+        Color::Red,
+        String::from("Africa"),
+        String::from("qwqwsa1dty_"),
+    );
+
+    println!("{:?}", form_output);
+    println!("{:?}", form_output.validate().unwrap());
+
+    form_output.first_name = String::from("");
+    println!("{:?}", form_output.validate().unwrap_err());
+
+    form_output.first_name = String::from("as");
+    form_output.password = String::from("dty_1");
+    println!("{:?}", form_output.validate().unwrap_err());
+
+    form_output.password = String::from("asdasASd(_");
+    println!("{:?}", form_output.validate().unwrap_err());
+
+    form_output.password = String::from("asdasASd123SA");
+    println!("{:?}", form_output.validate().unwrap_err());
 }
