edf04697d231af621ae984ff8c7c4b56ba9c98d5
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-19 10:37:36 +0200

    middle day test 3

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,12 @@ pub use chrono::TimeZone;
 
 
 fn main() {
-    let date = Utc.ymd(2011, 12, 2).and_hms(21, 12, 09);
-
     assert_eq!(wd::Tue, middle_day(2019).unwrap());
     assert_eq!(wd::Wed, middle_day(1997).unwrap());
     assert_eq!(wd::Mon, middle_day(1663).unwrap());
     assert_eq!(wd::Wed, middle_day(1873).unwrap());
     assert_eq!(wd::Thu, middle_day(1953).unwrap());
     assert_eq!(wd::Wed, middle_day(1879).unwrap());
-
 
     // println!("{:?}", middle_day(1022).unwrap());
 }
