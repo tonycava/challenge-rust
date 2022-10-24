48020a83c08dd7bc0f41a7e32305dda4776b447e
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-24 20:42:43 +0200

    macro calculator test 1

---
+++ macro_calculator/src/lib.rs
@@ -0,0 +1,39 @@
+pub struct Food {
+    pub name: String,
+    pub calories: [String; 2],
+    pub proteins: f64,
+    pub fats: f64,
+    pub carbs: f64,
+    pub nbr_of_portions: f64,
+}
+
+pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
+    if foods.len() == 0 {
+        return json::parse(r#"
+            {
+                "cals": 0.0,
+                "carbs": 0.0,
+                "proteins": 0.0,
+                "fats": 0.0
+            }
+            "#).expect("err in len");
+    }
+    let mut final_cals: f64 = 0.0;
+    let mut final_carbs: f64 = 0.0;
+    let mut final_proteins: f64 = 0.0;
+    let mut final_fats: f64 = 0.0;
+
+    for food in foods.iter()
+    {
+        final_cals += food.calories[1].replace("kcal", "").parse::<f64>().unwrap() * food.nbr_of_portions;
+        final_carbs += format!("{:.2}", final_carbs).parse::<f64>().unwrap();
+        final_proteins += format!("{:.2}", food.proteins * food.nbr_of_portions).parse::<f64>().unwrap();
+        final_fats += format!("{:.2}", food.fats * food.nbr_of_portions).parse::<f64>().unwrap();
+    }
+    final_cals = format!("{:.2}", final_cals).parse::<f64>().unwrap();
+    let json =
+        String::from("{") +
+            &format!(r#""cals": {},"carbs": {},"proteins": {},"fats": {}"#, final_cals, final_carbs, final_proteins, final_fats) +
+            "}";
+    json::parse(&json).expect("err in return")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-pub use does_it_fit::*;
+use macro_calculator::*;
 
-fn main() {
-    // println!(
-    //     "Do 100 rectangles (2x1) fit in a 2 by 4 square? {}",
-    //     area_fit(2, 4, GeometricalShapes::Rectangle, 100, 2, 1) // false
-    // );
-    // println!(
-    //     "Do 3 triangles (5 base and 3 height) fit in a 5 by 5 square? {}",
-    //     area_fit(5, 5, GeometricalShapes::Triangle, 3, 5, 3) // true
-    // );
-    // println!(
-    //     "Do 3 spheres (2 radius) fit in a 5 by 5 by 5 box? {}",
-    //     volume_fit(5, 5, 5, GeometricalVolumes::Sphere, 3, 2, 0, 0) // true
-    // );
-    // println!(
-    //     "Does 1 parallelepiped (6 base, 7 height and depth 4) fit in a 5 by 7 by 5 parallelepiped? {}",
-    //     volume_fit(5, 7, 5, GeometricalVolumes::Parallelepiped, 1, 6, 7, 4) // true
-    // );
-    println!("{}", volume_fit(2, 5, 3 , GeometricalVolumes::Parallelepiped, 1, 1, 1, 1))
+fn main(){
+    let a = vec![
+        Food {
+            name: String::from("big mac"),
+            calories: ["2133.84kJ".to_string(), "510kcal".to_string()],
+            proteins: 27.0,
+            fats: 26.0,
+            carbs: 41.0,
+            nbr_of_portions: 2.0,
+        },
+        Food {
+            name: "pizza margherita".to_string(),
+            calories: ["1500.59kJ".to_string(), "358.65kcal".to_string()],
+            proteins: 13.89,
+            fats: 11.21,
+            carbs: 49.07,
+            nbr_of_portions: 4.9,
+        },
+    ];
+
+    println!("{:#}", calculate_macros(a));
 }
