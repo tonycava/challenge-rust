bee8206afef48f903ef012bbd3adc72f282bfb54
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-07 08:10:12 +0100

    expected variable test 10

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -4,6 +4,7 @@ use case::*;
 pub fn expected_variable(compared: &str, excepted: &str) -> Option<String> {
     println!("{}", compared);
     println!("{}", excepted);
+    let mut needToPass = true;
     if compared.contains('-') || excepted.contains('-') || compared.contains(' ') || excepted.contains(' ') {
         println!("dashes");
         return None;
@@ -12,7 +13,8 @@ pub fn expected_variable(compared: &str, excepted: &str) -> Option<String> {
         println!("same");
         return Some("100%".to_string());
     }
-    if !compared.is_camel_lowercase() && !compared.is_camel_lowercase() {
+    if compared.contains("_") || excepted.contains("_") { needToPass = false; }
+    if !compared.is_camel_lowercase() && !compared.is_camel_lowercase() && needToPass {
         println!("not camel");
         return None;
     }
