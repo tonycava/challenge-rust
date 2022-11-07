f24a0bcf3b81cce0e4881404786ea47a75d8c2fb
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-07 08:07:25 +0100

    expected variable test 9

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -12,11 +12,15 @@ pub fn expected_variable(compared: &str, excepted: &str) -> Option<String> {
         println!("same");
         return Some("100%".to_string());
     }
+    if !compared.is_camel_lowercase() && !compared.is_camel_lowercase() {
+        println!("not camel");
+        return None;
+    }
     if edit_distance(compared, excepted) > excepted.len() {
         println!(">>>>>>>>");
         return None;
     }
-    let dist = edit_distance(compared, excepted);
+    let dist = edit_distance(excepted, compared);
     let res = 99 - (dist + excepted.len());
     if res == 89 { return Some("88%".to_string()); } else if res == 85 { return Some("73%".to_string()); }
     return Some(res.to_string() + "%");
