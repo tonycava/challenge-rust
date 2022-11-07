094511c45539370a715d2a06bee5cf55b43a7891
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-07 09:12:36 +0100

    mobs test 1

---
+++ mobs/src/lib.rs
@@ -0,0 +1,50 @@
+pub use crate::mobs::*;
+
+pub mod mobs;
+
+impl Boss {
+    pub fn new(&self, name: String, age: u8) -> Self {
+        Self {
+            name,
+            age,
+        }
+    }
+}
+
+impl Mob {
+    pub fn recruit(&mut self, name: String, age: u8) {
+        self.members.push(Member {
+            age,
+            name,
+            role: Role::Associate,
+        });
+    }
+    pub fn steal(&mut self, mob2: &mut Mob, attack: u32) {
+        self.wealth -= attack;
+        mob2.wealth += attack;
+    }
+
+    pub fn attack(&mut self, mob2: &mut Mob) {
+        let mut attack1 = 0;
+        let mut attack2 = 0;
+        for member in self.members.iter() {
+            attack1 += member.role.clone() as u8
+        }
+        for member in mob2.members.iter() {
+            attack2 += member.role.clone() as u8
+        }
+
+        println!("{attack1}");
+        println!("{attack2}");
+    }
+}
+
+impl Member {
+    pub fn new(&self, name: String, role: Role, age: u8) -> Self {
+        Self {
+            name,
+            role,
+            age,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-use expected_variable::*;
+pub use mobs::{Boss, Mob, Member};
+
 
 fn main() {
-    println!(
-        "{} close to it",
-        expected_variable("On_Point", "on_point").unwrap()
+    let (mafia1, mafia2) = (
+        Mob {
+            name: "Hairy Giants".to_string(),
+            boss: boss::new("Louie HaHa", 36),
+            cities: vec![("San Francisco".to_string(), 7)],
+            members: vec![
+                member::new("Benny Eggs", member::Role::Soldier, 28),
+                member::new("Jhonny", member::Role::Associate, 17),
+                member::new("Greasy Thumb", member::Role::Soldier, 30),
+                member::new("No Finger", member::Role::Caporegime, 32),
+            ],
+            wealth: 100000,
+        },
+        Mob {
+            name: "Red Thorns".to_string(),
+            boss: boss::new("Big Tuna", 30),
+            cities: vec![("San Jose".to_string(), 5)],
+            members: vec![
+                member::new("Knuckles", member::Role::Soldier, 25),
+                member::new("Baldy Dom", member::Role::Caporegime, 36),
+                member::new("Crazy Joe", member::Role::Underboss, 23),
+            ],
+            wealth: 70000,
+        },
     );
-    println!(
-        "{} close to it",
-        expected_variable("soClose", "So_Close").unwrap()
-    );
-    println!(
-        "{:?}",
-        expected_variable("something", "something_completely_different")
-    );
-    println!(
-        "{} close to it",
-        expected_variable("BenedictCumberbatch", "BeneficialCucumbersnatch").unwrap()
-    );
+
+    println!("{:?}\n{:?}", mafia1, mafia2);
 }
